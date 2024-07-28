0dfdb0ce75467d976f954ba4767048ebbd7a5ae1
notx123 <gitupmszx@gmail.com>
2024-07-28 18:50:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/poeepadikada/newvesionpaid.git /newvesionpaid
+  git clone https://github.com/notx123/santhos.git /notx123
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /newvesionpaid
+  git clone $UPSTREAM_REPO /notx123
 fi
-cd /newvesionpaid
+cd /notx123
 pip3 install -U -r requirements.txt
 echo "🚀Start⚡...."
 python3 bot.py
